7df09b4bfeb724cf4c3563cfeca4a6f094de0207
Fredy-lg20*** <fredythekkekkara@gmail.com>
2020-12-03 18:41:12 +0100

    product image

--- assets/components/addProductModal.php
+++ assets/components/addProductModal.php
@@ -1,7 +1,88 @@
 <div class="modal fade" id="addNewProduct">
-    <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
+                <script type="text/javascript">
+                    function readURL(input) {
+                        if (input.files && input.files.length > 0) {
+
+
+                            var i;
+                            for (i = 0; i < input.files.length; i++) {
+                                text += cars[i] + "<br>";
+                            }
+
+
+
+
+                            var reader = new FileReader();
+                            reader.onload = function (e) {
+                                $('#test').attr('src', e.target.result);
+                            }
+                            reader.readAsDataURL(input.files[0]);
+                        }
+                    }
+
+                    function addImage() {
+                        const div = document.createElement('div');
+
+                        div.className = 'row';
+
+                        div.innerHTML = `
+<input type="text" name="name" value="" />
+<input type="text" name="value" value="" />
+<label> 
+<input type="checkbox" name="check" value="1" /> Checked? 
+                    </label>
+<input type="button" value="-" onclick="removeRow(this)" />
+`;
+
+                        document.getElementById('content').appendChild(div);
+                    }
+
+                    function removeRow(input) {
+                        document.getElementById('content').removeChild(input.parentNode);
+                    }
+
+
+
+
+
+
+                    $(function() {
+                        // Multiple images preview in browser
+                        var imagesPreview = function(input, placeToInsertImagePreview) {
+
+                            if (input.files) {
+                                var filesAmount = input.files.length;
+
+                                for (i = 0; i < filesAmount; i++) {
+                                    var reader = new FileReader();
+
+                                    reader.onload = function(event) {
+                                        $($.parseHTML('<img>')).attr('src', event.target.result)
+                                            .attr('id', "test")
+                                            .appendTo(placeToInsertImagePreview);
+                                    }
+
+                                    reader.readAsDataURL(input.files[i]);
+                                }
+                            }
+
+                        };
+
+                        $('#gallery-photo-add').on('change', function() {
+                            imagesPreview(this, 'div.gallery');
+                        });
+                    });
+
+
+
+
+
+
+
+                </script>
                 <h5 class="modal-title">Add a new product</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">×</span>
@@ -31,7 +112,6 @@
                             <option>Seafood</option>
                             <option>Desserts</option>
                             <option>Cereals</option>
-
                             <option>Baked goods</option>
                             <option>Dried foods</option>
 
@@ -78,18 +158,53 @@
                             </select>
                         </div>
                     </div>
+
+
                     <div class="form-group">
+                        <label>Add product images</label>
+                        <div class="mx-4 justify-content-center align-middle row">
+                            <div class="gallery row"></div>
+                            <label class="btn btn-default rounded-circle" id="addBtn">
+                                <i class="fa fa-plus"></i> <input onchange="readURL(this);" type="file" hidden id="gallery-photo-add" name="files" multiple>
+                            </label>
+
+                        </div>
+                    </div>
+
+                    <div class="modal-footer justify-content-between">
+
+                        <div class="form-group">
                             <div class="form-check form-check-inline">
                                 <input class="form-check-input" type="checkbox" id="inlineCheckbox5" value="option1">
                                 <label class="form-check-label" for="inlineCheckbox5">It is a processed food.</label>
                             </div>
+                        </div>
+                        <div>
+                            <button type="submit" class="btn btn-primary">Save</button>
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        </div>
+
                     </div>
-                    <div class="modal-footer">
-                        <button type="submit" class="btn btn-primary">Save</button>
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    </div>
+
                 </form>
             </div>
+
         </div>
     </div>
 </div>
+
+
+
+<style>
+    #test{
+        width: 150px;
+        height: 150px;
+        margin: 10px;
+        object-fit: cover;
+    }
+    #addButton{
+        width: 50px;
+        height: 50px;
+        background-color: aqua;
+    }
+</style>
